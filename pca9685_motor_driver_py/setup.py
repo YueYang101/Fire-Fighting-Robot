5b4af71daf54634875a1d5f46dd0f11e3c20db62
YueYang101 <zcemuex@ucl.ac.uk>
2025-05-08 21:34:09 +0100

    setup py update

--- pca9685_motor_driver_py/setup.py
+++ pca9685_motor_driver_py/setup.py
@@ -1,25 +1,32 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+from glob import glob
+import os
 
-package_name = 'pca9685_motor_driver_py'
+package_name = "pca9685_motor_driver_py"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
+    version="0.0.0",
+    packages=[package_name],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ubuntu-robot-pi4',
-    maintainer_email='ubuntu-robot-pi4@todo.todo',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    author="Yang Yue",
+    author_email="zcemuex@ucl.ac.uk",
+    description="ROS 2 motor driver for a PCA9685 PWM board",
+    license="MIT",
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            # ros2 run pca9685_motor_driver_py motor_driver_node
+            f"motor_driver_node = {package_name}.motor_driver_node:main",
         ],
     },
+    # non-python resources that must be installed
+    data_files=[
+        ("share/ament_index/resource_index/packages",
+            [f"resource/{package_name}"]),
+        (f"share/{package_name}",              ["package.xml"]),
+        (f"share/{package_name}/launch",       glob("launch/*.py")),
+        (f"share/{package_name}/config",       glob("config/*.yaml")),
+        (f"share/{package_name}/srv",          glob("srv/*.srv")),
+    ],
 )
