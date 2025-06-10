5f8f1bad73516a7edf78c85e26657e91fe6b5110
YueYang101 <zcemuex@ucl.ac.uk>
2025-06-10 20:16:21 +0100

    fix system monitor

--- system_monitor/setup.py
+++ system_monitor/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/system_monitor_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -18,4 +19,10 @@ setup(
     description='ROS2 System Monitor for Raspberry Pi',
     license='MIT',
     tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'system_monitor_node = system_monitor.system_monitor_node:main',
+            'system_info_publisher = system_monitor.system_info_publisher:main',
+        ],
+    },
 )
